17cb17a7f209b5a5fc166fa50a441823f770016d
Ankit Sahu <98406547+sahuankit08@users.noreply.github.com>
2022-01-26 13:18:24 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,12 @@
+#Add Implementation!!
+def add(x,y):
+    return x+y
+
+#Subtract Implementation
+def subtract(x,y):
+    return x-y
+
+
+#Multiply Implementation
+def multiply(x,y):
+    return x*y
